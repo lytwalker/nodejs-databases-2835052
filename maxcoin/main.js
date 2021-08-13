7296630cda448999b852280fc792cb4279a289d5
Tichaona Brendon Nyeke <sleepingchickensoftware@gmail.com>
2021-08-13 18:45:17 +0200

    03_04 - Complete

--- maxcoin/main.js
+++ maxcoin/main.js
@@ -1,12 +1,12 @@
-const CoinAPI = require('./services/CoinAPI');
+const MongoBackend = require("./services/backend/MongoBackend");
 
 async function run() {
-    const coinAPI = new CoinAPI();
-    return coinAPI.fetch();
+  const mongoBackend = new MongoBackend();
+  return mongoBackend.max();
 }
 
 run()
-    .then((result) => {
-        console.log(result);
-    })
-    .catch((err) => console.error(err));
+  .then((result) => {
+    console.log(result);
+  })
+  .catch((err) => console.error(err));
--- maxcoin/services/backend/MongoBackend.js
+++ maxcoin/services/backend/MongoBackend.js
@@ -1,23 +1,72 @@
 /* eslint-disable no-useless-constructor */
 /* eslint-disable class-methods-use-this */
 /* eslint-disable no-empty-function */
-const CoinAPI = require('../CoinAPI');
+
+const { MongoClient } = require("mongodb");
+
+const CoinAPI = require("../CoinAPI");
 
 class MongoBackend {
+    constructor() {
+        this.coinAPI = new CoinAPI();
+        this.mongoUrl = "mongodb://localhost:37017/maxcoin";
+        this.client = null;
+        this.collection = null;
+    }
 
-  constructor() {
-    this.coinAPI = new CoinAPI();
-  }
+    async connect() {
+        const mongoClient = new MongoClient(this.mongoUrl, {
+            useUnifiedTopology: true,
+            useNewUrlParser: true,
+        });
+        this.client = await mongoClient.connect();
+        this.collection = this.client.db("maxcoin").collection("values");
+        return this.client;
+    }
 
-  async connect() {}
+    async disconnect() {
+        if (this.client) {
+            return this.client.close();
+        }
+        return false;
+    }
 
-  async disconnect() {}
+    async insert() {
+        const data = await this.coinAPI.fetch();
+        const documents = [];
+        Object.entries(data.bpi).forEach((entry) => {
+            documents.push({
+                date: entry[0],
+                value: entry[1],
+            });
+        });
+        return this.collection.insertMany(documents);
+    }
 
-  async insert() {}
+    async getMax() {}
 
-  async getMax() {}
+    async max() {
+        console.info("Connection to MongoDB");
+        console.time("mongodb-connect");
+        const client = await this.connect();
+        if (client.isConnected()) {
+            console.info("Successfully connected to MongoDB");
+        } else {
+            throw new Error("Connecting to MongoDB failed");
+        }
+        console.timeEnd("mongodb-connect");
 
-  async max() {}
+        console.info("Inserting into MongoDB");
+        console.time("mongodb-insert");
+        const insertResult = await this.insert();
+        console.timeEnd("mongodb-insert");
+        console.info(`Inserted ${insertResult.result.n} documents into MongoDB.`);
+
+        console.info("Disconnecting from MongoDB");
+        console.time("mongodb-disconnect");
+        await this.disconnect();
+        console.timeEnd("mongodb-disconnect");
+    }
 }
 
 module.exports = MongoBackend;
